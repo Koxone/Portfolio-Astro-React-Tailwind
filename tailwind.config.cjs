054eb3d125758dbbba09b709a5d78cc9c84f7cca
Carlos de Leon <admin@koxland.net>
2025-06-27 18:28:14 -0600

    Add Component Library project and support for new font
    Added a new 'Component Library' project with logo and description to Projects.astro. Updated Logo.astro to accept a custom font prop. Included Space Grotesk font in Layout.astro and Tailwind config. Commented out default body font in global.css.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{astro,html,js,jsx}",
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx}"],
   theme: {
     extend: {
-            boxShadow: ['group-hover'],
-      backgroundColor: ['group-hover'],
-      borderColor: ['group-hover'],
+      boxShadow: ["group-hover"],
+      backgroundColor: ["group-hover"],
+      borderColor: ["group-hover"],
       fontFamily: {
-        inter: ['Inter', 'sans-serif'],
-        pacifico: ['Pacifico', 'cursive'],
+        inter: ["Inter", "sans-serif"],
+        pacifico: ["Pacifico", "cursive"],
+        "space-grotesk": ["Space Grotesk", "sans-serif"],
       },
     },
   },
