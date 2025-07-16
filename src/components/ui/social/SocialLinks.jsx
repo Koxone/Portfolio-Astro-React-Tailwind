9c0e22d8f85a28018e72233bf5f2da3645454e8d
Carlos de Leon <admin@koxland.net>
2025-07-16 16:29:29 -0600

    Update project order and LinkedIn URL
    Reordered projects in Projects.astro for improved presentation and updated the LinkedIn profile URL in SocialLinks.jsx to the correct address.

--- src/components/ui/social/SocialLinks.jsx
+++ src/components/ui/social/SocialLinks.jsx
@@ -6,7 +6,7 @@ function SocialLinks() {
       <a href="https://github.com/Koxone" className="hover:flash">
         <img src="/assets/icons/github.png" alt="GitHub" className="w-9 h-full" />
       </a>
-      <a href="https://www.linkedin.com/in/juan-carlos-de-leon-ozuna-21580635b/" className="hover:flash">
+      <a href="https://www.linkedin.com/in/carlos-d-leon/" className="hover:flash">
         <img src="/assets/icons/linkedin.png" alt="LinkedIn" className="w-9 h-full" />
       </a>
       <a href="https://www.instagram.com/carlos.d.leon/" className="hover:flash">
