f1fad456bdd9c05e0585bd9178a6b9ac0431bc4a
Carlos de Leon <admin@koxland.net>
2025-07-19 21:01:39 -0600

    Add resume files and update social links
    Added resume.pdf and resume.svg to the public directory. Updated SocialLinks component to include a 'Download Resume' button. Changed MainTitle subtitle to 'Frontend Software Engineer Journey'. Removed SocialLinks from the index.astro page.

--- src/components/ui/social/SocialLinks.jsx
+++ src/components/ui/social/SocialLinks.jsx
@@ -3,6 +3,14 @@ import React from "react";
 function SocialLinks() {
   return (
     <div className="flex justify-center items-center gap-4">
+      <a
+        href="/resume.pdf"
+        download
+        className="hover:flash cursor-pointer border px-2 py-1.5 rounded-[9999px] bg-white text-black font-medium"
+      >
+        Download Resume
+      </a>
+
       <a href="https://github.com/Koxone" className="hover:flash">
         <img src="/assets/icons/github.png" alt="GitHub" className="w-9 h-full" />
       </a>
